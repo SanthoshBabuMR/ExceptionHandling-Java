40b33842736de02486d7671290cf074a29b4137a
Santhosh Babu M R <santhosh.infotec@gmail.com>
2019-03-09 13:43:29 +0530

    u

---
+++ src/com/babusa/assertions/B.java
@@ -0,0 +1,8 @@
+package com.babusa.assertions;
+
+public class B {
+
+    void test(int i) {
+        assert i > 0 : "invalid i in B.test";
+    }
+}
---
+++ src/com/babusa/assertions/p1/C.java
@@ -0,0 +1,11 @@
+package com.babusa.assertions.p1;
+
+import com.babusa.assertions.B;
+import com.babusa.assertions.D;
+
+public class C {
+
+    public void test(int i) {
+        assert i > 0 : "invalid i in C.test";
+    }
+}
